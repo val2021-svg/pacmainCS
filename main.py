ccc9b5d1e95165f04724e1af91e00194c11db4bf
Thomas Melwig <thomas.melwig@student-cs.fr>
2023-01-11 14:45:47 +0100

    fenetre d accueil

---
+++ main.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+import subprocess
+import settings
+
+def run_program():
+    subprocess.run(["python", "hand_tracking.py"])
+
+root = tk.Tk()
+root.title('PACMAIN CS')
+
+#creation of a canva
+canva = tk.Canvas(root, width=settings.sw, height=settings.sh, bg='blue')
+canva.pack()
+
+#creation of buttons
+startbttn = tk.Button(root, text='Start Game', command=run_program, width=15, height=2)
+
+#center coords
+x_startbttn = settings.sw / 2
+y_startbttn = settings.sh / 2
+
+#place the buttons
+canva.create_window(x_startbttn, y_startbttn, window=startbttn)
+
+
+menubar = tk.Menu(root)
+filemenu = tk.Menu(menubar, tearoff=0)
+filemenu.add_command(label="Run Program", command=run_program)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+root.config(menu=menubar)
+root.mainloop()

cabc21460895c3ae2d32ad85c01f8168f12859b4
Thomas Melwig <thomas.melwig@student-cs.fr>
2023-01-12 11:31:52 +0100

    random signs added

--- calcul.py
+++ calcul.py
@@ -3,21 +3,21 @@ import mediapipe as mp
 import settings
 from google.protobuf.json_format import MessageToDict
 
-#Computes the euclidian length between 2 points
+# Computes the euclidian length between 2 points
 def eucli_length(P1,P2):
     x1, y1, z1 = P1
     x2, y2, z2 = P2
     return sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
 
-#Computes the cosinus opposed to the 'a' segment with Alkashi's method
+# Computes the cosinus opposed to the 'a' segment with Alkashi's method
 def cos_alkashi(a,b,c):
     return (b**2 + c**2 - a**2)/(2*b*c)
 
-#Converts degrees into radians
+# Converts degrees into radians
 def radian(x):
     return x*2*pi/360
 
-#Creates an array composed of the 21 hand's landmarks 
+# Creates an array composed of the 21 hand's landmarks 
 def pos_hand_landmarks(image, results):
     for hand_landmarks in results.multi_hand_landmarks : 
         pos = []
@@ -26,34 +26,34 @@ def pos_hand_landmarks(image, results):
             pos.append((xi, yi, zi))
     return pos
 
-#Right or Left hand ? 
+# Right or Left hand ? 
 def handedness(results):
     for idx, hand_handedness in enumerate(results.multi_handedness):
                     handedness_dict = MessageToDict(hand_handedness)
                     whichHand =(handedness_dict['classification'][0]['label'])
                     return whichHand
 
-def little_wrap(pos): #level 1 of wrapping your fingers
+def little_wrap(pos): # level 1 of wrapping your fingers
     if pos[8][1] > pos[6][1] and pos[12][1] > pos[10][1] and pos[16][1] > pos[14][1] and pos[20][1] > pos[18][1]:
         return True
     else :
         return False
 
-def really_wrap(pos): #level2 of wrapping your fingers
+def really_wrap(pos): # level 2 of wrapping your fingers
     if pos[8][1] > pos[5][1] and pos[12][1] > pos[9][1] and pos[16][1] > pos[13][1] and pos[20][1] > pos[17][1]:
         return True
     else :
         return False
 
 def spread(pos):
-    #distances to the wrist
+    # distances to the wrist
     thumb = eucli_length(pos[0],pos[4])
     ind_fing = eucli_length(pos[0],pos[8])
     mid_fing = eucli_length(pos[0],pos[12])
     ring_fing = eucli_length(pos[0],pos[16])
     little_fing = eucli_length(pos[0],pos[20])
 
-    #distances between fingers
+    # distances between fingers
     thumb_to_ind = eucli_length(pos[4],pos[8])
     ind_to_mid = eucli_length(pos[8],pos[12])
     mid_to_ring = eucli_length(pos[12],pos[16])
@@ -69,7 +69,7 @@ def spread(pos):
 
     
 
-# #distances of the phalanxes 
+#     # distances of the phalanxes 
 #     a_thumb = calcul.eucli_length(pos[3],pos[1])
 #     b_thumb = calcul.eucli_length(pos[3],pos[2])
 #     c_thumb = calcul.eucli_length(pos[2],pos[1])
@@ -86,7 +86,7 @@ def spread(pos):
 #     b_little_fing = calcul.eucli_length(pos[19],pos[18])
 #     c_little_fing = calcul.eucli_length(pos[18],pos[17])
     
-#     #cosinus of the angles of the fingers
+#     # cosinus of the angles of the fingers
 #     thumb_angle = calcul.cos_alkashi(a_thumb,b_thumb,c_thumb)
 #     ind_angle = calcul.cos_alkashi(a_ind_fing,b_ind_fing,c_ind_fing)
 #     mid_angle = calcul.cos_alkashi(a_mid_fing,b_mid_fing,c_mid_fing)
--- sign.py
+++ sign.py
@@ -3,18 +3,19 @@ import calcul
 import settings
 from math import *
 import draw
+import random
 
 def victory(image, results):
 
     pos = calcul.pos_hand_landmarks(image,results)
     draw.draw_HAND(image, pos)
 
-    #Victory or not
+    # Victory or not
     a = calcul.eucli_length(pos[8],pos[12])
     b = calcul.eucli_length(pos[8],pos[0])
     c = calcul.eucli_length(pos[0],pos[12])            
 
-    #Right or Left hand
+    # Right or Left hand
     if calcul.handedness(results) == 'Right':
 
         if calcul.cos_alkashi(a,b,c) < cos(calcul.radian(10)) and calcul.cos_alkashi(a,b,c) > cos(calcul.radian(45)) and pos[12][0] > pos[8][0]:
@@ -30,21 +31,22 @@ def victory(image, results):
             cv2.putText(image, calcul.handedness(results), (settings.sh,80), 0, 1.5, settings.RED, 2)
             
 
-def fing_spread(image, results):
+def fing_spread(image, results, steps):
     
     cv2.putText(image, "Spread your fingers", (50,80), 0, 1, settings.RED, 2)
     cv2.putText(image, calcul.handedness(results), (settings.sh,80), 0, 1.5, settings.RED, 2)
     pos = calcul.pos_hand_landmarks(image,results)
     draw.draw_HAND(image, pos)
 
-    #spreaded or not
+    # spreaded or not
     if not calcul.little_wrap(pos):
         if calcul.spread(pos):
             cv2.putText(image, "GREAT!", (settings.sw//2,settings.sh//2), 0, 1, settings.GREEN, 6)
             for i in range(21):
                 draw.color_pos(image, pos[i], settings.RED)
+            steps[1] = 1
 
-def wrap_fing(image, results):
+def wrap_fing(image, results, steps):
 
     cv2.putText(image, "Wrap your fingers", (50,80), 0, 1, settings.RED, 2)
     cv2.putText(image, calcul.handedness(results), (settings.sh,80), 0, 1.5, settings.RED, 2)
@@ -56,6 +58,7 @@ def wrap_fing(image, results):
             cv2.putText(image, "MAGNIFICENT!", (settings.sw//2 - 30,settings.sh//2), 0, 1, settings.GREEN, 6)
             for i in range(21):
                 draw.color_pos(image, pos[i], settings.RED)
+            steps[1] = 1
         
         elif calcul.little_wrap(pos):
             cv2.putText(image, "GREAT!", (settings.sw//2 - 20,settings.sh//2), 0, 1, settings.GREEN, 6)
@@ -70,11 +73,11 @@ def arpege(image, results, steps):
     pos = calcul.pos_hand_landmarks(image,results)
     draw.draw_HAND(image, pos)
     
-    #Condition of the cycle's end
+    # Condition of the cycle's end
     if steps[6] == 1 : 
         draw.draw_HAND(image, pos)
 
-    #step1
+    # step1
     if steps[0] == 0 :
         draw.color_pos(image, pos[4], settings.RED)
         draw.color_pos(image, pos[6], settings.RED)
@@ -83,7 +86,7 @@ def arpege(image, results, steps):
         draw.draw_HAND(image, pos)
         steps[0] = 1
         
-    #step2
+    # step2
     if steps[0] == 1 and steps[1] == 0:
         draw.color_pos(image, pos[4], settings.RED)
         draw.color_pos(image, pos[8], settings.RED)
@@ -91,7 +94,7 @@ def arpege(image, results, steps):
             draw.draw_HAND(image, pos)
             steps[1] = 1
     
-    #step3
+    # step3
     if steps[1] == 1 and steps[2] == 0:
         draw.color_pos(image, pos[4], settings.RED)
         draw.color_pos(image, pos[12], settings.RED)
@@ -99,7 +102,7 @@ def arpege(image, results, steps):
             draw.draw_HAND(image, pos)
             steps[2] = 1
     
-    #step4
+    # step4
     if steps[2] == 1 and steps[3] == 0:
         draw.color_pos(image, pos[4], settings.RED)
         draw.color_pos(image, pos[16], settings.RED)
@@ -107,7 +110,7 @@ def arpege(image, results, steps):
             draw.draw_HAND(image, pos)
             steps[3] = 1
 
-    #step4
+    # step4
     if steps[3] == 1 and steps[4] == 0:
         draw.color_pos(image, pos[4], settings.RED)
         draw.color_pos(image, pos[20], settings.RED)
@@ -115,7 +118,7 @@ def arpege(image, results, steps):
             draw.draw_HAND(image, pos)
             steps[4] = 1
     
-    #step5
+    # step5
     if steps[4] == 1 and steps[5] == 0:
         draw.color_pos(image, pos[4], settings.RED)
         draw.color_pos(image, pos[19], settings.RED)
@@ -123,13 +126,14 @@ def arpege(image, results, steps):
             draw.draw_HAND(image, pos)
             steps[5] = 1
     
-    #step6
+    # step6
     if steps[5] == 1 and steps[6] == 0:
         draw.color_pos(image, pos[4], settings.RED)
         draw.color_pos(image, pos[17], settings.RED)
         if calcul.eucli_length(pos[4],pos[13]) <= 50 :
             draw.draw_HAND(image, pos)
             steps[6] = 1
+            # return 'end'
 
             
 def thumb_mouv(image, results, steps):
@@ -142,16 +146,16 @@ def thumb_mouv(image, results, steps):
 
     if steps[1] == 1 :
         draw.draw_HAND(image, pos)
-    #the red dot
+    # the red dot
     if steps[0] == 0 : 
         draw.color_pos(image, pos[17], settings.RED)
 
-    #closed thumb
+    # closed thumb
     if steps[0] == 0 and calcul.eucli_length(pos[4], pos[17]) < 60 : 
         draw.draw_HAND(image, pos)
         steps[0] = 1
     
-    #opened thumb
+    # opened thumb
     if steps[0] == 1 and steps[1] == 0 :
         cv2.putText(image, "Then stretch your thumb", (50,110), 0, 0.7, settings.RED, 2)
         draw.color_HAND(image, pos, settings.RED)
@@ -159,8 +163,36 @@ def thumb_mouv(image, results, steps):
         if thumb_angle < cos(calcul.radian(35)) and thumb_angle > cos(calcul.radian(90)) :
             if calcul.handedness(results)=='Right' and pos[4][0] < pos[5][0]:
                 steps[1] = 1
+                # return 'end'
             elif calcul.handedness(results)=='Left' and pos[4][0] > pos[5][0]:
                 steps[1] = 1
+                # return 'end'
+
+# randomizes the order of the exercises
+def random_signs(sign_list):
+    rd_list = []
+
+    while len(sign_list) != 0:
+        i = random.randint(0,len(sign_list)-1)
+        rd_list.append(sign_list[i])
+        del sign_list[i]
+
+    return rd_list
+
+# finds the correct function to execute
+def find_sign_func(sign_name, image, results, steps):
+    if sign_name == 'fing_spread':
+        return fing_spread(image, results, steps)
+    
+    elif sign_name == 'wrap_fing':
+        return wrap_fing(image, results, steps)
+
+    elif sign_name == 'arpege':
+        return arpege(image, results, steps)
+    
+    elif sign_name == 'thumb_mouv':
+        return thumb_mouv(image, results, steps)
+
 
 
 
